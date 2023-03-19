421f79a5dd9886dafdcc4c9f0eb3d31a97a73dae
Thenu Kaluarachchi <thenujaya123@gmail.com>
2023-03-19 18:08:37 +0000

    chkp

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +21,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+// Auth test
+Route::get('/posts', function(Request $request)
+{
+    $user = Auth::check();
+    // if user is not logged in return 401
+    if (!$user) {
+        return response('Unauthorized.', 401);
+    }
+    $response = [
+        'posts' => Post::all()
+    ];
+    return response($response, 200);
 });
